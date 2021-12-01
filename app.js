503dc3de90225523bf8f0c7e2ba58dc10427478d
mahabub alam <mahabu998@gmail.com>
2021-12-01 22:38:16 +0600

    first commit bycrpt and jwt add

---
+++ app.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+const port = 5000
+
+dotenv.config()
+app.use(express.json())
+
+require('./db/conn')
+// const User = require('./model/userSchema')
+// link to the router files to make our route easy
+app.use(require('./router/auth'))
+
+// console.log(DB)
+
+
+
+//middelware
+const middleware = (req,res,next) => {
+  console.log("hallo my middleware")
+  next()
+} 
+// middleware()
+
+app.get('/', (req, res) => {
+  res.send('Hello world i ma a app.js')
+})
+
+app.get('/about',middleware,(req, res) => {
+  console.log("hallo my about")
+  res.send('Hello world about!')
+})
+app.get('/contact', (req, res) => {
+  res.send('Hello world about contact!')
+})
+app.get('signing', (req, res) => {
+  res.send('Hello world signing!')
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ router/auth.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const bcrypt = require('bcrypt');
+const router = express.Router();
+require("../db/conn");
+const User = require("../model/userSchema");
+
+router.get("/", (req, res) => {
+  res.send("Hello world i ma a auth.js");
+});
+
+//promies funcation
+
+// router.post("/register", (req, res) => {
+//   const { name, email, phone, work, password, cpassword } = req.body;
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(422).json({ error: "filed i not compeilte" });
+//   }
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(422).json({ error: "email allready exist" });
+//       }
+//       const user = new User({ name, email, phone, work, password, cpassword });
+//       user
+//         .save()
+//         .then(() => {
+//           res.status(201).json({ message: "user register successfully" });
+//         })
+//         .catch((error) =>
+//           res.status(500).json({ error: "failed to registered" })
+//         );
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+
+// });
+
+// async awite
+
+router.post("/register", async (req, res) => {
+  const { name, email, phone, work, password, cpassword } = req.body;
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "filed i not compeilte" });
+  }
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "email allready exist" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: "password are not matching" });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+      await user.save();
+      res.status(201).json({ message: "user register successfully" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.post("/singin", async (req, res) => {
+  try {
+    let token; 
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "plz filed the data" });
+    }
+    const userLogin = await User.findOne({ email: email });
+    console.log(userLogin);
+ 
+    if(userLogin){
+      const isMatch = await bcrypt.compare(password, userLogin.password)
+
+    const token = await userLogin.generateAuthToken();
+    console.log(token)
+
+      if (isMatch) {
+        res.status(400).json({ error: "Invalid Credientials pass" });
+      } else {
+        res.json({ message: "user singing Successfully" });
+      }
+    }else{
+      res.status(400).json({ error: "Invalid Credientials " });
+    }
+
+ 
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
